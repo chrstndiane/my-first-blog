d5e27360c727832c851420cbcb5555f530a73c9d
chrstndiane <christinedianecuenca@gmail.com>
2025-02-25 19:44:54 +0800

    created separated folder for new blog page

--- blog/views.py
+++ blog/views.py
@@ -13,4 +13,4 @@ def post_detail(request, pk):
 
 def new_blog(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/new_blog.html', {'posts': posts})
+    return render(request, 'new/new_blog.html', {'posts': posts})
