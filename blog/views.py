36eed5c252c99a64516532e2a73bfaa5aaa1b318
chrstndiane <christinedianecuenca@gmail.com>
2025-02-25 12:07:25 +0800

    added urls

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.http import HttpResponse
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def say_hello(request):
+    return HttpResponse('Hello Django')
